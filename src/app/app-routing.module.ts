378c143e2f98a06fe889fa83aa79e25888c0d098
wdb3ar <wdb3ar@gmail.com>
2019-03-31 07:31:18 +0600

    login page, product page, router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { CatalogPageComponent } from './catalog-page/catalog-page.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CategoryListComponent } from './category-list/category-list.component';
 
-const routes: Routes = [];
+const catalogRoutes: Routes = [
+  { path: 'product-list', component: ProductListComponent },
+  { path: 'category-list', component: CategoryListComponent }
+];
+
+const routes: Routes = [
+  { path: 'login', component: LoginPageComponent },
+  { path: 'catalog', component: CatalogPageComponent, children: catalogRoutes }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { FormModalComponent } from './form-modal/form-modal.component';
+import { CategoryListComponent } from './category-list/category-list.component';
+import { CatalogPageComponent } from './catalog-page/catalog-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    ProductListComponent,
+    FormModalComponent,
+    CategoryListComponent,
+    CatalogPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
